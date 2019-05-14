7a17f882a95886e464a0975f092cd511dd88bffd
sounak <sounakmukherjee@ymail.com>
2019-05-14 13:58:01 -0400

    event delegations backup1

--- app.js
+++ app.js
@@ -4,15 +4,41 @@ class EventHandlers{
         this.detail= document.getElementById('detail');
         this.value= document.getElementById('value');
         this.add= document.getElementById('submit');
+        this.incomeUL=document.querySelector('.inc');
+        this.expenseUL= document.querySelector('.exp');
     }
 
     eventListeners=(ui,db)=>{
+        // when the add button is clicked
         this.add.addEventListener('click',()=>{
             
             db.setTransaction(this.type.value,this.detail.value,this.value.value);
                          
             ui.displayData(this.type.value,this.detail.value,this.value.value);
+
+            
+        this.clearFields();
+
         });
+
+        // when the list elements are clicked
+        // this.incomeUL=document.querySelector('.inc');
+        // this.expenseUL= document.querySelector('.exp');
+
+        this.incomeUL.addEventListener('click',(e)=>{
+            console.log('Inside the Income UL',e.target);
+            this.eventDelegation(e)});
+        this.expenseUL.addEventListener('click',e=>{
+            console.log('inside the expense UL',e.target);   
+            this.eventDelegation(e)});
+    }
+    clearFields=()=>{
+        this.detail.value='';
+        this.value.value='';
+    }
+
+    eventDelegation=(e)=>{
+        console.log(' Inside the delegation emthod');
     }
 }
 
@@ -41,7 +67,8 @@ class UIPainter{
     displayData=(type,detail,value)=>{
 
         const li=document.createElement('li');
-        li.innerHTML = `<strong>${detail}&nbsp;&nbsp;&nbsp;${value}<strong>`;
+        li.innerHTML = `<strong>${detail}&nbsp;&nbsp;&nbsp;${value}<strong>
+                    <button>delete</button><button>edit</button>`;
 
         document.querySelector(`.${type}`).insertAdjacentElement('beforeend',li);
 
